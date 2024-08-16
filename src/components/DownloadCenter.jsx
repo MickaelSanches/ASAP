04318a610aa7e21e18e5f19e6210fa7342146415
MikEight <mickael.4870@gmail.com>
2024-08-16 16:01:19 +0200

    editing text at DownloadCenter.jsx

--- src/components/DownloadCenter.jsx
+++ src/components/DownloadCenter.jsx
@@ -6,8 +6,7 @@ const DownloadCenter = () => {
       <div className="relative bg-white shadow-lg p-8 w-11/12 max-w-4xl text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">CENTRE DE TÉLÉCHARGEMENT</h2>
         <p className="text-lg mb-8">
-          Vous voulez consulter <strong>les catalogues</strong> en ligne ? Besoin <strong>d'une notice</strong> pour vos produits ?
-        </p>
+          Vous voulez consulter <strong>les catalogues</strong> en ligne ?</p>
         <div className="relative flex justify-center items-center gap-8">
           <div className="absolute bg-violet shadow-lg p-8 w-full max-w-md text-center transform translate-x-2 translate-y-1/2">
             <img src="/img/logo.webp" alt="Catalogues" className="w-full h-auto mb-4" />
